91479ea06f5c34d585341e88a44495ffb325ace8
helenohyeah <ouyang.helen@gmail.com>
2020-09-28 21:20:19 +0000

    Added post to get user input on new url form

--- express_server.js
+++ express_server.js
@@ -13,10 +13,6 @@ const urlDatabase = {
 
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
@@ -34,6 +30,14 @@ app.get('/urls/:shortURL', (req, res) => {
   res.render('urls_show', templateVars);
 });
 
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('OK');
+})
+
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
 
 // app.get("/urls.json", (req, res) => {
 //   res.json(urlDatabase);
