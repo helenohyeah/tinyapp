be589f1800b66f1130de20d7cdc862c76e22ad7e
helenohyeah <ouyang.helen@gmail.com>
2020-09-28 20:18:41 +0000

    Set up a view on /urls

--- express_server.js
+++ express_server.js
@@ -4,13 +4,19 @@ const PORT = 8080; // default port 8080
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
+  "43hb2E": "http://www.spotify.com"
 };
 
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
